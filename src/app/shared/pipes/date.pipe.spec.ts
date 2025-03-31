2fabf660bcaba0c5dbdc690f802209ba94dd8670
Gál József <h260710@stud.u-szeged.hu>
2025-03-31 09:02:35 +0200

    Add pipe

---
+++ src/app/shared/pipes/date.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DateFormatterPipe } from './date.pipe';
+
+describe('DateFormatterPipe', () => {
+  it('create an instance', () => {
+    const pipe = new DateFormatterPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
